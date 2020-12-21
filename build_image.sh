f18afc3cbc21c36b013fa11fc6a7b9f3b173fcc4
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2020-12-21 22:57:18 +0000

    remove redundant codes

--- build_image.sh
+++ build_image.sh
@@ -11,7 +11,7 @@ DEFAULT_BASE_GPU_IMAGE="nvidia/cuda:10.1-cudnn8-runtime-ubuntu18.04"
 DEFAULT_BASE_CPU_IMAGE="ubuntu:18.04"
 
 readonly -a BUILD_TARGET=(
-  cpu
+#   cpu
   gpu
 )
 readonly -a targets=(${WHAT:-${BUILD_TARGET[@]}})
@@ -22,16 +22,16 @@ readonly -a cpu_image=(${BASE_CPU_IMAGE:-${DEFAULT_BASE_CPU_IMAGE}})
 
 function build_docker_images(){
   for t in "${targets[@]}"; do
-#     if [ "$t" == "gpu" ]; then
-#       BASE_IMAGE=${gpu_image}
-#       RECSYS_IMAGE="cambridgeltl/glances:${GIT_VERSION}-${GIT_COMMIT}-gpu"
-#     else
-#       BASE_IMAGE=${cpu_image}
-#       RECSYS_IMAGE="cambridgeltl/glances:${GIT_VERSION}-${GIT_COMMIT}"
-#     fi
+    if [ "$t" == "gpu" ]; then
+      BASE_IMAGE=${gpu_image}
       RECSYS_IMAGE="cambridgeltl/glances:${GIT_VERSION}-${GIT_COMMIT}-gpu"
+    else
+      BASE_IMAGE=${cpu_image}
+      RECSYS_IMAGE="cambridgeltl/glances:${GIT_VERSION}-${GIT_COMMIT}"
+    fi
+      BASE_IMAGE=${gpu_image}
     docker build --no-cache --build-arg BASE_IMAGE="$BASE_IMAGE" -t "${RECSYS_IMAGE}" -f "${RECSYS_ROOT}/Dockerfile" ${RECSYS_ROOT}
   done
 }
-
+echo "build result: ${RECSYS_IMAGE}"
 build_docker_images
