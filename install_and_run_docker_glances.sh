bb49542baa42638f119c212f2c973a334ad01fba
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2021-01-16 17:38:12 +0000

    ADD non-GPU MACHINE DOCKER IMAGE

--- install_and_run_docker_glances.sh
+++ install_and_run_docker_glances.sh
@@ -1,3 +1,4 @@
+##### Install Docker ######
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -6,17 +7,28 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt update
 apt-get install docker-ce docker-ce-cli containerd.io
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-sudo apt update
-sudo apt install nvidia-docker2
-sudo systemctl restart docker
+gpu_test=$(lspci |grep -i nvidia)
+gpu_str="NVIDIA"
+if [[ $gpu_test =~ $gpu_str ]] ; then
+  echo "========================FOUND NVIDIA GPU========================="
+        ##### Install Nvidia-docker ######
+        distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+        curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+        curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
+        sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+        sudo apt update
+        sudo apt install nvidia-docker2
+        sudo systemctl restart docker
+        wget https://raw.githubusercontent.com/cambridgeltl/glances/develop/glances-docker.service -O /etc/systemd/system/glances-docker.service
+else
+  echo "=====================No NVIDIA GPU CAN BE FOUND============================"
+  wget https://raw.githubusercontent.com/cambridgeltl/glances/develop/glances-docker-cpu.service -O /etc/systemd/system/glances-docker.service
+fi
+
+##### Start Server ######
 systemctl restart docker
-wget https://raw.githubusercontent.com/cambridgeltl/glances/develop/glances-docker.service -O /etc/systemd/system/glances-docker.service
 systemctl enable glances-docker
 systemctl start glances-docker
 echo "======================================================"
